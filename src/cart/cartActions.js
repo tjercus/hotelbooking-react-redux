44f5d4d2798a1fabab3627133625e4a9c77a12fa
tjerk.valentijn <tjerk.valentijn@ebpi.nl>
2019-05-23 14:58:58 +0200

    add item button

--- src/cart/cartActions.js
+++ src/cart/cartActions.js
@@ -1,7 +1,14 @@
-import { DEL_ITEM_CMD } from "./cartConstants";
+import { DEL_ITEM_CMD, ADD_ITEM_CMD } from "./cartConstants";
 
 export const delItemAction = item => ({
   type: DEL_ITEM_CMD,
+  payload: {
+    item
+  }
+});
+
+export const addItemAction = item => ({
+  type: ADD_ITEM_CMD,
   payload: {
     item
   }
--- src/offers/OfferRowComponent.js
+++ src/offers/OfferRowComponent.js
@@ -1,12 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { addItemAction } from "../cart/cartActions";
+
 const OfferRowComponent = ({ offer }) =>
   offer ? (
     <tr key={offer.id}>
       <td>{offer.name}</td>
       <td>{offer.price}</td>
-      <td>add and remove</td>
+      <td><button onClick={addItemAction}>add to cart</button></td>
     </tr>
   ) : null;
 
